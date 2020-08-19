6117ce3dacdf3470690caa61c5404b188fac0e6e
FNuri <fowsihashi@hotmail.co.uk>
2020-08-19 13:51:40 +0300

    added redirect file

--- src/tourData.js
+++ src/tourData.js
@@ -6,7 +6,7 @@ export const tourData = [
     img: "https://images.unsplash.com/photo-1564676487888-8be98153b4a6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=629&q=80",
     name: "london royal palace tour",
     info:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+      "The London Royal Palace Pass is your passport to travel back to a time when royalty reigned the United Kingdom. Get a free, one-time access to 4 historic royal palaces - Tower of London, Hampton Court Palace, Kensington Palace and Banqueting House and learn all about the years of British monarchy. This Pass is designed to give you a royal London experience and combines the best London sightseeing, beauty, history and art resulting in an authentic British royal experience!. With this Pass you have the liberty to explore the palaces independently and visit the Royal Palaces at your convenience without being restricted to a fixed itinerary. There's no better pass for a person looking to delve into the British history!"
   },
   
   {
@@ -15,7 +15,7 @@ export const tourData = [
     img: "https://images.unsplash.com/photo-1582584169118-29f43e8b2261?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
     name: "Kirkstall Abbey tour",
     info:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+      "Founded over 800 years ago in 1152, Kirkstall Abbey is a feat of historic architecture set amid a haven of wildlife and greenery. Feel a true sense of the past as you stroll through its tranquil grounds, surrounded by stunning tree-filled parkland on the banks of the River Aire. One of the most complete Cistercian monasteries in Britain, the abbey is a marvel to behold with its dramatic architecture. Visitors can explore the history of Kirkstall Abbey at our interactive Visitor Centre; it sits on the site of the Reredorter, the name given to the toilets used by the laybrothers who carried out agricultural and manual tasks at the abbey!"
   },
   {
     id: 3,
@@ -24,7 +24,7 @@ export const tourData = [
     img: "https://images.unsplash.com/photo-1597588795310-d7e799ca9ca2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
     name: "The Needles tour",
     info:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+      "Choose between a 20 minute slow cruise to view the Needles Rocks and Lighthouse, coloured sands of Alum Bay, dramatic chalk cliff face and the surrounding coastal features or a 15 minute fast trip on the High Speed Rib. Whichever option you choose, you’re guaranteed a breath-taking Isle of Wight experience."
   },
   
   {
@@ -33,7 +33,7 @@ export const tourData = [
     img: "https://www.biographyonline.net/wp-content/uploads/2016/03/sherwood-forest-xxl-Major_Oak.jpg.webp",
     name: "Sherwood Forest tour",
     info:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+      "A landmark thousands of years in the making, alive with nature, history and folklore, and its all yours to explore, from its ancient oaks and legendary characters, to the work of the RSPB, protecting the forest today. Play, discover and leave with tales of your own, inspired by 420 hectares of beautiful, precious, unique woodland."
       
   },
 
@@ -42,7 +42,7 @@ export const tourData = [
     city: "Kent",
     img: "https://images.unsplash.com/photo-1486401292818-ecc554452678?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=749&q=80",
     name: "The White Cliffs of Dover tour",
-    info:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"  },
+    info:"Take in the majestic view of the English Channel and look far to see the French coast on a clear day from the cliffs of the rugged English shoreline. The white chalk cliffs were formed during the Cretaceous Period and reach a height of 300 feet. Remnants of the sites' importance in defense during both world wars can still be seen including trenches and former base end stations. White Cliffs of Dover has the South Foreland Lighthouse, Shakespeare Cliff, the SS Falcon ship wreck, chalk grassland and the Langdon Convict Prison remains."  },
 
 
   
